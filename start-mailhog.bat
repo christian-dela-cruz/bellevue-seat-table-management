cffb9838a2dcf995807d3301dbf3bbb4ace6930a
Christian Dela Cruz <christian07.19.17delacruz@gmail.com>
2026-06-01 13:23:08 +0800

    chore: add MailHog local testing config and startup script

---
+++ start-mailhog.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting MailHog with persistent storage...
+
+:: Create the data folder if it doesn't exist
+if not exist "mailhog-data" mkdir "mailhog-data"
+
+:: Start MailHog
+.\MailHog.exe -storage maildir -maildir-path .\mailhog-data
